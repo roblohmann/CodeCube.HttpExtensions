bd69343c05011131560cae99b91fa54defcc6cfe
Rob Lohmann <roblohman@gmail.com>
2019-12-23 13:01:56 +0100

    added initial version of extension

---
+++ CodeCube.HttpExtensions/HttpClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CodeCube.HttpExtensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<HttpResponseMessage> DeleteWithBodyAsync(this HttpClient httpClient, Uri requestUri, StringContent requestContent)
+        {
+            var requestMessage = new HttpRequestMessage
+            {
+                    Method = HttpMethod.Delete,
+                    Content = requestContent,
+                    RequestUri = requestUri
+            };
+
+            return await httpClient.SendAsync(requestMessage).ConfigureAwait(false);
+        }
+    }
+}
