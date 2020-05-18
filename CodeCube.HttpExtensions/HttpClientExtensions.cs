de9e809797f257f978b04c37445ab3df7595ca9b
Rob Lohmann <rob@ecare.nl>
2020-05-18 11:33:22 +0200

    added support for cancellation token

--- CodeCube.HttpExtensions/HttpClientExtensions.cs
+++ CodeCube.HttpExtensions/HttpClientExtensions.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CodeCube.HttpExtensions
 {
     public static class HttpClientExtensions
     {
-        public static async Task<HttpResponseMessage> DeleteWithBodyAsync(this HttpClient httpClient, Uri requestUri, StringContent requestContent)
+        /// <summary>
+        /// Send an asynchronous DELETE-request with a requestbody to the specified endpoint.
+        /// </summary>
+        /// <param name="httpClient">The HTTP-Client</param>
+        /// <param name="requestUri">The request URI.</param>
+        /// <param name="requestContent">The content for the DELETE-request</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or thread to receive notification of cancellation.</param>
+        /// <returns></returns>
+        public static async Task<HttpResponseMessage> DeleteWithBodyAsync(this HttpClient httpClient, Uri requestUri, StringContent requestContent, CancellationToken cancellationToken)
         {
             var requestMessage = new HttpRequestMessage
             {
-                    Method = HttpMethod.Delete,
-                    Content = requestContent,
-                    RequestUri = requestUri
+                Method = HttpMethod.Delete,
+                Content = requestContent,
+                RequestUri = requestUri
             };
 
-            return await httpClient.SendAsync(requestMessage).ConfigureAwait(false);
+            return await httpClient.SendAsync(requestMessage, cancellationToken).ConfigureAwait(false);
         }
     }
 }
